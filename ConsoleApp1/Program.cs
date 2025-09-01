09508a3c71f7ac2f1818f7249cd84de5bf47c114
samerayari <s235677@dtu.dk>
2025-09-01 16:47:51 +0200

    tilføjet kodning

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, venner!");
+Console.WriteLine("Hello, venner!");int Control(int altitude)
+
+{
+    int thruster = 0;
+    
+    if (altitude > 100) thruster = 0; 
+    else if (altitude > 0) thruster = 1;
+    else thruster = 0;
+    
+    return thruster;
+}
+
+void Test(int altitude)
+{
+    int thruster = Control(altitude);
+    bool behaviorCorrect = (altitude > 100 && thruster == 0) ||
+                           (altitude is <= 100 and > 0 && thruster == 1) ||
+                           (altitude <= 0 && thruster == 0);
+    var behaviorCorrectIcon = behaviorCorrect ? "✅" : "❌";
+    Console.WriteLine($"For altitude {altitude}, your thruster is {thruster} |{behaviorCorrectIcon}|");
+}
+
+Test(150);
+Test(100);
+Test(50);
+Test(0);
+Test(-1);
+
+
